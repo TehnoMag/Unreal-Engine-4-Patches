cfc9691dfc264345696406a8044797de09c6f3da
TehnoMag <delta-server@ya.ru>
2018-12-24 14:57:15 +0300

    Added fix for Linux host and QTCreator IDE

--- projectfix.sh
+++ projectfix.sh
@@ -5,22 +5,40 @@ ENGINE_PATH="E\:\\\Unreal Engine 4\\\Engine\\\Engine"
 #Place you project name
 PROJECT="MyProject"
 #Place you project module names with space as delimeter
-MODULES="Module1 Module2"
+MODULES="Module1 Module 2"
+PLATFORMS="Win64 Win32 Linux"
 
 echo $PROJECT
 echo $ENGINE_PATH
 
+#for Linux host found platform ID in Intermeidate directory
+PLATFORM_ID=$(ls Intermediate/Build/Linux/ | grep -v Editor)
+echo $PLATFROM_ID
+
 #STEP1 Fixing Includes Search paths for generated files (Engine Issue)
 
 PRJFILE="Intermediate/ProjectFiles/${PROJECT}.vcxproj"
+QTCFILE="${PROJECT}Includes.pri"
 
 for module in `echo $MODULES`;
 do
-	sed -i "s/${PROJECT}Editor\\\Inc\\\\${module}/UE4Editor\\\Inc\\\\${module}/g" ${PRJFILE}
+	echo "Fixing Path for module ${module}"
+	sed -i "s/${PROJECT}Editor\\\Inc\\\\${module}/UE4Editor\\\Inc\\\\${module}/g" ${PRJFILE} 2>/dev/null
+	sed -i "s/${PROJECT}Editor\/Inc\/${module}/UE4Editor\/Inc\/${module}/g" ${QTCFILE} 2>/dev/null
 done
 
-sed -i "s/\.\.\\\Build\\\Win64\\\\${PROJECT}Editor/${ENGINE_PATH}\\\Intermediate\\\Build\\\Win64\\\UE4Editor/g" ${PRJFILE}
-sed -i "s/\\\\${PROJECT}Editor\\\/\\\UE4Editor\\\/g" ${PRJFILE}
+for platform in `echo $PLATFORMS`;
+do
+	[ "$(uname)" == "Linux" ] && plat_id="\/${PLATFORM_ID}\/" || plat_id=""
+
+	echo "Fixing Path for platoform ${platform}"
+	sed -i "s/\.\.\\\Build\\\\${platform}\\\\${PROJECT}Editor/${ENGINE_PATH}\\\Intermediate\\\Build\\\\${platform}\\\UE4Editor/g ${PRJFILE}" 2>/dev/null
+	sed -i "s/Intermediate\/Build\/${platform}${plat_id}${PROJECT}Editor/${ENGINE_PATH}\/Intermediate\/Build\/${platform}${plat_id}UE4Editor" ${QTCFILE} 2>/dev/null
+done
+
+
+sed -i "s/\\\\${PROJECT}Editor\\\/\\\UE4Editor\\\/g" ${PRJFILE} 2>/dev/null
+sed -i "s/\/${PROJECT}Editor\//\/UE4Editor\//g" ${QTCFILE} 2>/dev/null
 
 #STEP2 Fixing encoding page to UTF-8 (Git issue)
 
